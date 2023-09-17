71633577441b81811e26580ab79717cd19e9f0c7
Divinewill Zero <zerothebahdman@users.noreply.github.com>
2023-09-17 22:11:00 +0100

    fix: minor bug fix

--- src/http/router/v1/router.module.ts
+++ src/http/router/v1/router.module.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import authRoute from './auth.route';
 import UserRoute from './user.route';
+import paymentRoute from './payment.route';
 
 const router = Router();
 
 const defaultRoutes = [
   { path: '/auth', route: authRoute },
   { path: '/user', route: UserRoute },
+  { path: '/payment', route: paymentRoute },
 ];
 
 defaultRoutes.forEach(({ path, route }) => {
